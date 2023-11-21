8073ae3c6faa37a99b7f5664b79adfdaf84498ae
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-11-20 23:01:03 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ echo "Enabling firewall..."
 sudo ufw enable
 echo "Enabled firewall."
 
+sleep 0.1
 #daily updates
 
 echo "Configuring updates..."
@@ -14,7 +15,7 @@ echo 'APT::Periodic::AutocleanInterval "1";' | sudo tee -a /etc/apt/apt.conf.d/1
 echo 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee -a /etc/apt/apt.conf.d/10periodic
 echo "Automatic updates have been configured to run daily."
 
-
+sleep 0.1
 #set password for ALL (just sets to current user password which is almost guaranteed to be a secure one)
 echo "Setting up passwords..."
 CURRENT_USER_PASSWORD=$(grep $(whoami) /etc/shadow | cut -d':' -f2)
@@ -23,12 +24,12 @@ for USER in $(getent passwd | cut -d: -f1); do
     echo "Password for user '$USER' has been set to the current user's password."
 done
 echo "Passwords done."
-
+sleep 0.1
 #disable ipv4 forwarding
 echo "Disabling IPv4 Forwarding..."
 sudo echo 0 > /proc/sys/net/ipv4/ip_forward
 echo "Disabled IPv4 Forwarding."
-
+sleep 0.1
 #update app
 echo "Updating applications..."
 if command -v apt-get &>/dev/null; then
@@ -43,3 +44,4 @@ fi
 
 
 echo "Update completed successfully."
+sleep 0.1
